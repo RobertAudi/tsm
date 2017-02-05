543b69f7927b689477322803c03517cd72f383fb
Robert Audi <robert.audii@gmail.com>
2017-02-05 14:41:13 +0100

    Cleanup the directory structure
    - Rename the 'lib' directory to 'src'
    - Move the zsh completion file to the 'share/zsh' directory
    - Move the stable script in the 'dist/bin' directory instead of the 'dist' directory
    - Move build and release scripts to the 'tools' directory

--- tools/build.zsh
+++ tools/build.zsh
@@ -4,12 +4,12 @@ function {
   emulate -LR zsh
   setopt extended_glob clobber
 
-  local __tsm_script_name="tsm"
-  local __tsm_root="${${(%):-%x}:A:h}"
+  local __tsm_script_filename="tsm"
+  local __tsm_root="${${(%):-%x}:A:h:h}"
   local __tsm_build_dir="${__tsm_root}/build"
 
   command mkdir "${__tsm_build_dir}" &>/dev/null
   builtin print -l -- "${__tsm_root}"/lib/{shebang,setup,{core,utils,helpers,commands}/*,main,tsm}.zsh(.Nf:u+rw:) \
-    | command xargs command awk 'FNR==1 && NR > 1 {print ""}{print}' > "${__tsm_build_dir}/${__tsm_script_name}" \
-    && command chmod +x "${__tsm_build_dir}/${__tsm_script_name}" &>/dev/null
+    | command xargs command awk 'FNR==1 && NR > 1 {print ""}{print}' > "${__tsm_build_dir}/${__tsm_script_filename}" \
+    && command chmod +x "${__tsm_build_dir}/${__tsm_script_filename}" &>/dev/null
 }
--- tools/release.zsh
+++ tools/release.zsh
@@ -4,19 +4,19 @@ function {
   emulate -LR zsh
   setopt extended_glob clobber
 
-  local __tsm_script_name="tsm"
-  local __tsm_root="${${(%):-%x}:A:h}"
+  local __tsm_script_filename="tsm"
+  local __tsm_root="${${(%):-%x}:A:h:h}"
   local __tsm_build_dir="${__tsm_root}/build"
   local __tsm_dist_dir="${__tsm_root}/dist"
   local __tsm_build_script="${__tsm_root}/build.zsh"
 
-  if [[ ! -s "${__tsm_build_dir}/${__tsm_script_name}" ]]; then
+  if [[ ! -f "${__tsm_build_dir}/${__tsm_script_filename}" ]]; then
     "${__tsm_build_script}" || return $status
   fi
 
   {
     command mkdir "${__tsm_dist_dir}"
-    command rm -f "${__tsm_dist_dir}/${__tsm_script_name}"
-    command cp "${__tsm_build_dir}/${__tsm_script_name}" "${__tsm_dist_dir}"
+    command rm -f "${__tsm_dist_dir}/${__tsm_script_filename}"
+    command cp "${__tsm_build_dir}/${__tsm_script_filename}" "${__tsm_dist_bin_dir}/${__tsm_script_filename}"
   } &>/dev/null
 }
