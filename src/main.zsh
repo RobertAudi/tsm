07d06983e9ec4a00d3d8084e853259ecbdace011
Robert Audi <robert.audii@gmail.com>
2017-02-06 13:26:33 +0100

    Add a tool to generate a hidden 'tsm' subcommand

--- src/main.zsh
+++ src/main.zsh
@@ -2,6 +2,7 @@
 
 function __tsm::main() {
   local cmd="$1"
+  [[ "$cmd" == "tsm" ]] && { __tsm::tsm ; return $status }
   if (( ${+__tsm_commands[(r)$cmd]} )); then
     __tsm::"$cmd" "${@:2}"
   else
---
+++ tools/generate_logo_command.zsh
@@ -0,0 +1,28 @@
+#!/usr/bin/env zsh
+
+emulate -LR zsh
+setopt clobber
+
+function {
+local __tsm_root="${${(%):-%x}:A:h:h}"
+local __tsm_function_filename="tsm.zsh"
+local __tsm_function_dir="${__tsm_root}/src/commands"
+
+local __tsm_logo_string="$(builtin print -- "tsm" | figlet -f standard -w60 -c | toilet --gay -f term | sed -e 's/[[:space:]]*$//')"
+local __tsm_title_string="$(builtin print -- "Tmux Session Manager" | figlet -f term -w60 -c | toilet --gay -f term | sed -e 's/[[:space:]]*$//')"
+local __tsm_subtitle_string="$(builtin print -- "Save and restore the state of tmux sessions and windows." | figlet -f term -w60 -c | toilet --gay -f term | sed -e 's/[[:space:]]*$//')"
+
+{
+cat <<EOF
+function __tsm::tsm() {
+  cat <<"End-of-message"
+${__tsm_logo_string}
+
+${__tsm_title_string}
+${__tsm_subtitle_string}
+
+End-of-message
+}
+EOF
+} > "${__tsm_function_dir}/${__tsm_function_filename}"
+}
