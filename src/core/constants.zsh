35390adc12ad291f80baff785f6b6dcbe62f80d0
Robert Audi <robert.audii@gmail.com>
2017-02-07 18:33:28 +0100

    Add the underline style to colors

--- src/core/constants.zsh
+++ src/core/constants.zsh
@@ -3,18 +3,19 @@
 
 local -A colors
 colors=(
-  info    "$(tput setaf 32)"
-  warn    "$(tput setaf 221)"
-  error   "$(tput setaf 196)"
-  fatal   "$(tput setaf 7; tput setab 196)"
-  success "$(tput setaf 34)"
-  white   "$(tput setaf 7)"
-  red     "$(tput setaf 1)"
-  green   "$(tput setaf 2)"
-  blue    "$(tput setaf 4)"
-  dimmed  "$(tput setaf 245)"
-  bold    "$(tput bold)"
-  reset   "$(tput sgr0)"
+  info      "$(tput setaf 32)"
+  warn      "$(tput setaf 221)"
+  error     "$(tput setaf 196)"
+  fatal     "$(tput setaf 7; tput setab 196)"
+  success   "$(tput setaf 34)"
+  white     "$(tput setaf 7)"
+  red       "$(tput setaf 1)"
+  green     "$(tput setaf 2)"
+  blue      "$(tput setaf 4)"
+  dimmed    "$(tput setaf 245)"
+  bold      "$(tput bold)"
+  underline "$(tput smul)"
+  reset     "$(tput sgr0)"
 )
 readonly -l colors
 
