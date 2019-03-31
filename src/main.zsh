4c0f14cb436a40c9f507a63afa9b8d32469ad5b7
Robert Audi <robert.audii@gmail.com>
2019-03-31 11:42:36 +0200

    Show an error message when a command is not found

--- src/main.zsh
+++ src/main.zsh
@@ -6,6 +6,7 @@ function __tsm::main() {
   if [[ -n "$cmd" ]] && (( ${+__tsm_commands[$cmd]} )); then
     __tsm::commands::"$cmd" "${@:2}"
   else
+    __tsm::utils::log error "Command not found: $(__tsm::utils::colorize bold,white "$cmd")"
     __tsm::commands::help
     return 1
   fi
