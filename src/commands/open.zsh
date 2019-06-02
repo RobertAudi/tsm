223f0031153e202a2d56e15f36ae1f5ae77deaaa
Robert Audi <robert.audii@gmail.com>
2019-06-02 08:39:04 +0200

    Remove the open command

--- src/commands/open.zsh
+++
@@ -1,9 +0,0 @@
-# Restore a session and attach to one
-# Alias of: __tsm::commands::resume
-function __tsm::commands::open() {
-  __tsm::utils::log warn \
-    "The $(__tsm::utils::colorize warn "open") command" \
-    "is $(__tsm::utils::colorize bold,underline "DEPRECATED")," \
-    "use the $(__tsm::utils::colorize info "reusme") command instead."
-  __tsm::commands::resume "$@"
-}
