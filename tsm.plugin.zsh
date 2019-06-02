4c91edef2cecf768d9eaa1712196328397a74229
Robert Audi <robert.audii@gmail.com>
2019-06-02 11:41:01 +0200

    Export tsm env vars when sourcing the plugin file

--- tsm.plugin.zsh
+++ tsm.plugin.zsh
@@ -1,3 +1,11 @@
+typeset -gx TSM_HOME TSM_SESSIONS_DIR TSM_BACKUPS_DIR TSM_DEFAULT_SESSION_FILE TSM_BACKUPS_COUNT
+
+: ${TSM_HOME:=${XDG_DATA_HOME:-$HOME/.local/share}/tsm}
+: ${TSM_SESSIONS_DIR:=$TSM_HOME/sessions}
+: ${TSM_BACKUPS_DIR:=$TSM_HOME/backups}
+: ${TSM_DEFAULT_SESSION_FILE:=$TSM_HOME/default-session.txt}
+: ${TSM_BACKUPS_COUNT:=20}
+
 typeset __tsm_dist_dir="${${(%):-%x}:A:h}/dist"
 path=("${__tsm_dist_dir}/"bin $path)
 fpath=("${__tsm_dist_dir}/"functions $fpath)
