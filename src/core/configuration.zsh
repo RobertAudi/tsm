72e9ad0b18e9c4fe0ce0799be31b9ab001c40d75
Robert Audi <robert.audii@gmail.com>
2019-06-02 10:40:55 +0200

    Remove the legacy environment variables

--- src/core/configuration.zsh
+++ src/core/configuration.zsh
@@ -1,9 +1,6 @@
 # |Configuration| {{{
 # ------------------------------------------------------------------------------
 
-: ${TSM_LEGACY_HOME=$HOME/.tmux/tmux-sessions}
-: ${TSM_LEGACY_SESSIONS_DIR:=$TSM_LEGACY_HOME/sessions}
-
 : ${TSM_HOME:=${XDG_DATA_HOME:-$HOME/.local/share}/tmux-sessions}
 : ${TSM_SESSIONS_DIR:=$TSM_HOME/sessions}
 : ${TSM_BACKUPS_DIR:=$TSM_HOME/backups}
